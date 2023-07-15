8d47b377096cf2f61e6fcf8850ed6041cebb596e
4321ba <nevenincs007@gmail.com>
2023-07-15 17:11:57 +0200

    mccopy screenshot, make clean

--- buildrun.sh
+++ buildrun.sh
@@ -5,6 +5,8 @@ mkdir build
 cd build
 # generáljuk ki cmake-kel a build fájlokat a szülő könyvtárból
 cmake ..
+# letöröljük az előzőt, ha le lett cserélve a Skeleton.cpp, akkor újrafordítsa
+make clean
 # buildeljük a programot
 make
 #visszalépünk a textúrák mappájába, hogy a szorgalmi is jól fusson
